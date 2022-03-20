d4ec589d70e773876f188a8f3b9881e9021dea54
OmelianLevkovych <omelianai@gmail.com>
2022-03-20 14:59:45 +0200

    minor cleanup added skeleton for activities controller API

---
+++ src/GetTogether.API/Controllers/ActivitiesController.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace GetTogether.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActivitiesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ActivityEntity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+    }
+}
