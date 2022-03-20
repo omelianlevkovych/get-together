367bc4ed63dd91b187147f4d3e0a5c9ddaadd7b4
$sudo whoami <omelianai@gmail.com>
2022-03-20 15:00:16 +0200

    Backend skeleton (#1)
    * minor cleanup
    
    * added db context
    
    * added initinal db migration script
    
    * seed database
    
    * make database seeding async in startup
    
    * minor cleanup
    added skeleton for activities controller API

--- src/GetTogether.API/Program.cs
+++ src/GetTogether.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Seed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +10,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<SeedDatabase>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
+await SeedDatabase(app);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +35,19 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static async Task SeedDatabase(IHost host)
+{
+    using var scope = host.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
+
+    await context.Database.MigrateAsync();
+
+    var service = services.GetService<SeedDatabase>();
+    if (service == null)
+    {
+        throw new ArgumentNullException(nameof(service));
+    }
+    await service.Seed();
+}
---
+++ src/Persistence/Seed/SeedDatabase.cs
@@ -0,0 +1,57 @@
+﻿using Domain.Entities;
+
+namespace Persistence.Seed
+{
+    public class SeedDatabase
+    {
+        private readonly ApplicationDbContext context;
+
+        public SeedDatabase(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task Seed()
+        {
+            await SeedActivities();
+        }
+
+        private async Task SeedActivities()
+        {
+            if (context.Activities is null)
+            {
+                throw new ArgumentNullException(nameof(context.Activities));
+            }
+            if (context.Activities.Any())
+            {
+                // TODO: Log
+                return;
+            }
+
+            var activities = new List<ActivityEntity>
+            {
+                new ActivityEntity
+                {
+                    Title = "UkraineVictory",
+                    Date = DateTime.UtcNow.AddMonths(-1),
+                    Description = "Activity a month ago",
+                    Category = "drinks",
+                    City = "Kiev",
+                    Venue = "Central Pub",
+                },
+
+                new ActivityEntity
+                {
+                    Title = "FutureActivity1",
+                    Date = DateTime.UtcNow.AddMonths(1),
+                    Description = "Activity in a month from now",
+                    Category = "music",
+                    City = "Lviv",
+                    Venue = "Arena Lviv",
+                }
+            };
+            await context.Activities.AddRangeAsync(activities);
+            await context.SaveChangesAsync();
+        }
+    }
+}
