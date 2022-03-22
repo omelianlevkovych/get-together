075947d110276869f459128e86c353cbb9714c24
OmelianLevkovych <omelianai@gmail.com>
2022-03-22 17:32:53 +0200

    minor cleanup

--- src/Persistence/Interfaces/IApplicationDbContext.cs
+++ src/Persistence/Interfaces/IApplicationDbContext.cs
@@ -6,8 +6,6 @@ namespace Persistence.Interfaces
     public interface IApplicationDbContext
     {
         DbSet<ActivityEntity> Activities { get; set; }
-
-        // TODO: remove default, should just have cancellationToken param
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
