dbf74e743d5ce72949d54342904793d9b1bcca2d
OmelianLevkovych <omelianai@gmail.com>
2022-03-23 23:51:23 +0200

    updated activity form now we can see the fileds populated in the form when opening

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
@@ -24,15 +24,20 @@ export default function ActivityForm({activity: selectedActivity, closeForm}: Pr
         console.log(activity);
     }
 
+    function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) {
+        const {name, value} = event.target;
+        setActivity({...activity, [name]:value});
+    }
+
     return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete='off'>
-                <Form.Input placeholder='Title' value={activity.title} name='title' />
-                <Form.TextArea placeholder='Desciption' />
-                <Form.Input placeholder='Category' />
-                <Form.Input placeholder='Date' />
-                <Form.Input placeholder='City' />
-                <Form.Input placeholder='Venue' />
+                <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange} />
+                <Form.TextArea placeholder='Desciption' value={activity.description} name='description' onChange={handleInputChange} />
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange} />
+                <Form.Input placeholder='Date' value={activity.date} name='date' onChange={handleInputChange} />
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button floated='right' positive type='submit' content='Submit' />
                 <Button onClick={closeForm} floated='right' positive type='button' content='Cancel' />
             </Form>
