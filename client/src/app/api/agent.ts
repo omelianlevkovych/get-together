c1468abc7db343fa45c6ad16ffdc33ef244cae05
OmelianLevkovych <omelianai@gmail.com>
2022-03-25 01:27:00 +0200

    extended agent with other endpoints

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
+import { request } from 'http';
 import { ActivityViewModel } from "../models/activities";
+import { Activity } from '../models/activity';
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -29,7 +31,11 @@ const requests = {
 }
 
 const Activities = {
-    list: () => requests.get<ActivityViewModel>('/activities')
+    list: () => requests.get<ActivityViewModel>('/activities'),
+    details: (id: string) => requests.get<Activity>(`/activities/${id}`),
+    create: (activity: Activity) => requests.post<void>('activities', activity),
+    update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.delete<void>(`/activities/${id}`)
 }
 
 const agent = {
