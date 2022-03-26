a98acb6a6f78093e89728ad024399f930fb517f9
OmelianLevkovych <omelianai@gmail.com>
2022-03-26 15:56:21 +0200

    creating an activity using mobx

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
+import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore {
     activities: Activity[] = [];
@@ -50,5 +51,40 @@ export default class ActivityStore {
 
     closeForm = () => {
         this.editMode = false;
+    }
+
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuid();
+        try {
+            await agent.Activities.create(activity);
+            runInAction(() => {
+                this.activities.push(activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try{
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                this.activities.filter(x => x.id !== activity.id);
+                this.activities.push(activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch(error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
     }
 }
--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -9,12 +9,11 @@ import ActivityList from './ActivityList';
 
 interface Props {
     activities: Activity[];
-    createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
     submitting: boolean;
 }
 
-export default observer(function ActivityDashboard({activities, createOrEdit, deleteActivity, submitting}: Props) {
+export default observer(function ActivityDashboard({activities, deleteActivity, submitting}: Props) {
     const {activityStore} = useStore();
 
     const {selectedActivity, editMode} = activityStore;
@@ -31,10 +30,7 @@ export default observer(function ActivityDashboard({activities, createOrEdit, de
                 {selectedActivity && !editMode &&
                 <ActivityDetails />}
                 {editMode &&
-                <ActivityForm
-                    createOrEdit={createOrEdit}
-                    submitting={submitting}
-                />}
+                <ActivityForm/>}
             </Grid.Column>
         </Grid>
     )
