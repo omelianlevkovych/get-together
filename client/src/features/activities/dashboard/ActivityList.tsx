c857fcb84b6b9205d5bdb8f18a56cb9437992e49
OmelianLevkovych <omelianai@gmail.com>
2022-03-26 09:58:28 +0200

    fixed bug: when on deleting, all buttons are spinning

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 
@@ -10,6 +10,13 @@ interface Props {
 }
 
 export default function ActivityList({activities, selectActivity, deleteActivity, submitting}: Props) {
+    const [target, setTarget] = useState('');
+    
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+        setTarget(e.currentTarget.name);
+        deleteActivity(id);
+    }
+    
     return (
         <Segment>
             <Item.Group divided>
@@ -25,8 +32,13 @@ export default function ActivityList({activities, selectActivity, deleteActivity
                             <Item.Extra>
                                 <Button onClick={() => selectActivity(activity.id)}
                                 floated='right' content='View' color='blue' />
-                                <Button loading={submitting} onClick={() => deleteActivity(activity.id)}
-                                floated='right' content='Delete' color='red' />
+                                <Button
+                                name={activity.id} 
+                                loading={submitting && target === activity.id} 
+                                onClick={(e) => handleActivityDelete(e, activity.id)}
+                                floated='right' 
+                                content='Delete' 
+                                color='red' />
                                 <Label basic content={activity.category} />
                             </Item.Extra>
                         </Item.Content>
