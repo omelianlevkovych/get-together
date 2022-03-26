3c5179bb8befc78688ca45cae3671ac0e5887f9b
OmelianLevkovych <omelianai@gmail.com>
2022-03-26 14:40:22 +0200

    setting up mobx

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import React, { Fragment, useContext, useEffect, useState } from 'react';
 import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
@@ -6,8 +6,12 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import {v4 as uuid} from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './loadingComponent';
+import { useStore } from '../stores/store';
 
 function App() {
+  const {activityStore} = useStore();
+
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -81,6 +85,7 @@ function App() {
       <NavBar openForm={handleFormOpen}/>
 
       <Container style={{marginTop: '7em'}}>
+        <h2>{activityStore.title}</h2>
       <ActivityDashboard 
         activities={activities}
         selectedActivity={selectedActivity}
