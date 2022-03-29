82c21c3b67b13f33b37f80174cac845bf34b8164
$sudo whoami <omelianai@gmail.com>
2022-03-29 20:42:20 +0300

    Client routing - v1 (#10)
    * npm i rxjs-compat
    
    * added home route for home page, activities, and creat activity
    
    * minor refactoring
    
    * added navBar links
    
    * getting an individual activity (for detail page)
    
    * using route parameters

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -8,7 +8,7 @@ export default class ActivityStore {
     selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadInitial = true;
+    loadingInitial = true;
 
     constructor() {
         makeAutoObservable(this)
@@ -21,12 +21,11 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
+        this.loadingInitial = true; 
         try {
             const activitiesVM = await agent.Activities.list();
             activitiesVM.activities.forEach(activity => {
-                activity.date = activity.date.split('T')[0];
-                // mutating state here
-                this.activityDictionary.set(activity.id, activity);
+                this.setActivity(activity);
             })
             this.setLoadingInitial(false);
             
@@ -35,27 +34,37 @@ export default class ActivityStore {
             this.setLoadingInitial(false);
         }
     }
+
+    loadActivity = async (id: string) => {
+        let activity = this.getActivity(id);
+        if (activity) {
+            this.selectedActivity = activity;
+        } {
+            this.setLoadingInitial(true);
+            try {
+                activity = await agent.Activities.details(id);
+                this.setActivity(activity);
+                this.selectedActivity = activity;
+                this.setLoadingInitial(false);
+            } catch(error) {
+                console.log(error);
+                this.setLoadingInitial(false);
+            }
+        }
+    }
+
+    private setActivity = (activity: Activity) => {
+        activity.date = activity.date.split('T')[0];
+        // mutating state here
+        this.activityDictionary.set(activity.id, activity);
+    }
+
+    private getActivity = (id: string) => {
+        return this.activityDictionary.get(id);
+    }
     // in order not to use runInAction https://mobx.js.org/actions.html
     setLoadingInitial = (state: boolean) => {
-        this.loadInitial = state;
-    }
-
-    selectActivity = (id: string) => {
-        this.selectedActivity = this.activityDictionary.get(id);
-    }
-
-    cancelSelectedActivity = () => {
-        this.selectedActivity = undefined;
-    }
-
-    // id when we creating an activity, and no param when updating
-    openForm = (id?: string) => {
-        id ? this.selectActivity(id) : this.cancelSelectedActivity();
-        this.editMode = true;
-    }
-
-    closeForm = () => {
-        this.editMode = false;
+        this.loadingInitial = state;
     }
 
     createActivity = async (activity: Activity) => {
@@ -99,7 +108,6 @@ export default class ActivityStore {
             runInAction(() => {
                 this.activityDictionary.delete(id);
                 // remove the selected (activity detail) when activity is deleted
-                this.removeSelectedAtivity(id);
                 this.loading = false;
             })
         } catch(error) {
@@ -107,12 +115,6 @@ export default class ActivityStore {
             runInAction(() => {
                 this.loading = false;
             })
-        }
-    }
-
-    removeSelectedAtivity(id: string) {
-        if (this.selectedActivity && this.selectedActivity.id === id) {
-            this.cancelSelectedActivity();
         }
     }
 }
