f0371ff172f89d96974cf3aadda274af82a0d378
Omelian Levkovych <omelianai@gmail.com>
2022-03-31 21:50:38 +0300

    redirect to activity detail after activity create sumbission

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-import {v4 as uuid} from 'uuid';
+
 
 export default class ActivityStore {
     activityDictionary = new Map<string, Activity>();
@@ -73,7 +73,6 @@ export default class ActivityStore {
 
     createActivity = async (activity: Activity) => {
         this.loading = true;
-        activity.id = uuid();
         try {
             await agent.Activities.create(activity);
             runInAction(() => {
